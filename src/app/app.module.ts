29b99fd1e93182b035cbc13739243c622aed0384
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2019-05-21 13:43:36 +0200

    Added the last version of bootstrap. Created the remplate of the webb app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CvComponent } from './cv/cv.component';
+import { PersonalProjectsComponent } from './personal-projects/personal-projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
+const appRoutes: Routes = [
+  { path: 'home' , component: HomeComponent},
+  { path: 'cv' , component: CvComponent},
+  { path: 'personalProjects' , component: PersonalProjectsComponent},
+  { path: 'hobbies' , component: HobbiesComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CvComponent,
+    PersonalProjectsComponent,
+    HobbiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
